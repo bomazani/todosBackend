f6f7d79c5d32c5e4d8f0874cd29fa6a24a4ef5df
Bob Hunt <bob.hunt.email@gmail.com>
2018-07-26 16:09:42 -0400

    redirected root

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+const todos = ['repay Edwin someday', 'punch Morgan'];
+
+
+
 
 app.use(cors());
 
-app.get('/',(req, res) => {
-    res.send('Message');
+app.get('/', (req, res)=> {
+    res.redirect('/todos')
+})
+
+app.get('/todos',(req, res) => {
+    res.json(todos);
 });
 
 app.listen(port, () => {
